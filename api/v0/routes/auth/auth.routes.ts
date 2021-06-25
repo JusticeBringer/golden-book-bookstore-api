f8f84f63891f88e273afd8d5376a5100da1d159b
justicebringer <gabithebigg@gmail.com>
2021-06-26 00:52:42 +0300

    feat: Add confirmation route

--- api/v0/routes/auth/auth.routes.ts
+++ api/v0/routes/auth/auth.routes.ts
@@ -1,22 +1,54 @@
 import express, { Router, Request, Response } from 'express';
+import mongoose from 'mongoose';
 import { UserModel } from '../../../../database/models/user/user.model';
 import { IUserInput, IUser } from '../../../../database/models/user/user.interface';
 
 import {
   registerValidation,
   loginValidation,
-  emailValidation,
+  isVerifiedEmailInDatabase,
   hashPassword,
   passwordCompare,
-  createJwtToken
+  createJwtToken,
+  sendConfirmationEmail,
+  isEmailInDatabase
 } from '../../../../util/validation/validation';
+import { dotEnvConfig } from '../../../../dotenv/config';
+import jwt from 'jsonwebtoken';
 
 export const authRouter: Router = express.Router();
 
+authRouter.get(
+  '/register/confirmation/:token',
+  async (req: Request, res: Response): Promise<any> => {
+    const VALIDATION_EMAIL_SECRET = dotEnvConfig.VALIDATION_EMAIL_SECRET;
+    const loginUrl = dotEnvConfig.ORIGIN_DOMAIN_URL + '/signin';
+    const user = jwt.verify(req.params.token, VALIDATION_EMAIL_SECRET) as {
+      userId: string;
+      iat: number;
+      exp: number;
+    };
+
+    const userObjectId = mongoose.Types.ObjectId(user.userId);
+
+    await UserModel.updateOne({ _id: userObjectId }, { $set: { isVerifiedEmail: true } }).catch(
+      error => {
+        res.status(400).send(error);
+      }
+    );
+    res.send(
+      `<div style="padding: 5vw"><p style="font-size:5vw;margin-top:2vh"> Email confirmat.</p>  <a href="${loginUrl}" style="font-size:5vw;margin-top:2vh">Spre autentificare. </a></div>`
+    );
+  }
+);
+
 authRouter.post('/register/email', async (req: Request, res: Response): Promise<any> => {
+  console.log(req);
+  const userFromReqBody = req.body.user;
+
   const userForValidation: IUserInput = {
-    email: req.body.email,
-    password: req.body.password
+    email: userFromReqBody.email,
+    password: userFromReqBody.password
   };
 
   // validating data
@@ -26,8 +58,8 @@ authRouter.post('/register/email', async (req: Request, res: Response): Promise<
   }
 
   // check if user is already in database
-  const emailExists = await emailValidation(userForValidation.email);
-  if (emailExists) {
+  const verifiedEmailExists = await isVerifiedEmailInDatabase(userForValidation.email);
+  if (verifiedEmailExists) {
     return res.status(400).send('Email-ul este deja utilizat');
   }
 
@@ -44,6 +76,12 @@ authRouter.post('/register/email', async (req: Request, res: Response): Promise<
 
   try {
     const savedUser = await userModel.save();
+
+    // send confirmation email
+    await sendConfirmationEmail(savedUser._id, userForValidation.email).catch(error => {
+      res.status(400).send(error);
+    });
+
     res.send({ user: savedUser._id });
   } catch (error) {
     res.status(400).send(error);
@@ -51,9 +89,11 @@ authRouter.post('/register/email', async (req: Request, res: Response): Promise<
 });
 
 authRouter.post('/register/google', async (req: Request, res: Response): Promise<any> => {
+  const userFromReqBody = req.body.user;
+
   const userForValidation: IUserInput = {
-    email: req.body.email,
-    password: req.body.password
+    email: userFromReqBody.email,
+    password: userFromReqBody.password
   };
 
   // validating data
@@ -63,8 +103,8 @@ authRouter.post('/register/google', async (req: Request, res: Response): Promise
   }
 
   // check if user is already in database
-  const emailExists = await emailValidation(userForValidation.email);
-  if (emailExists) {
+  const verifiedEmailExists = await isVerifiedEmailInDatabase(userForValidation.email);
+  if (verifiedEmailExists) {
     return res.status(400).send('Email-ul este deja utilizat');
   }
 
@@ -88,9 +128,11 @@ authRouter.post('/register/google', async (req: Request, res: Response): Promise
 });
 
 authRouter.post('/login', async (req: Request, res: Response): Promise<any> => {
+  const userFromReqBody = req.body.user;
+
   const userForValidation: IUserInput = {
-    email: req.body.email,
-    password: req.body.password
+    email: userFromReqBody.email,
+    password: userFromReqBody.password
   };
 
   // validating data
@@ -99,10 +141,20 @@ authRouter.post('/login', async (req: Request, res: Response): Promise<any> => {
     return res.status(400).send(error.details[0].message);
   }
 
+  // check if email is in db
+  const emailInDatabase = await isEmailInDatabase(userForValidation.email);
+
+  // check if email is verified
+  const verifiedEmailExists = await isVerifiedEmailInDatabase(userForValidation.email);
+
+  if (emailInDatabase && !verifiedEmailExists) {
+    return res.status(400).send('Email inexistent sau nu a fost confirmat');
+  }
+
   // get user
   const user = await UserModel.findOne({ email: userForValidation.email });
 
-  // hash password
+  // compare passwords
   const validPass = await passwordCompare(userForValidation.password, user.password);
 
   if (!user || !validPass) {
--- util/validation/validation.ts
+++ util/validation/validation.ts
@@ -2,9 +2,10 @@ import Joi from 'joi';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { UserModel } from '../../database/models/user/user.model';
-import { IUserInput } from '../../database/models/user/user.interface';
+import { IUser, IUserInput } from '../../database/models/user/user.interface';
 import { Request, Response, NextFunction } from 'express';
 import { dotEnvConfig } from '../../dotenv/config';
+import mailgun from 'mailgun-js';
 
 export const registerValidation = (data: any) => {
   const schema = Joi.object<IUserInput>({
@@ -24,8 +25,26 @@ export const loginValidation = (data: any) => {
   return schema.validate(data);
 };
 
-export const emailValidation = async (email: string) => {
-  return await UserModel.findOne({ email: email });
+export const isEmailInDatabase = async (email: string) => {
+  const userByEmail: IUser = await UserModel.findOne({ email: email });
+  if (!userByEmail) {
+    return false;
+  }
+
+  return true;
+};
+
+export const isVerifiedEmailInDatabase = async (email: string) => {
+  const userByEmail: IUser = await UserModel.findOne({ email: email });
+  if (!userByEmail) {
+    return false;
+  }
+
+  if (!userByEmail.isVerifiedEmail) {
+    return false;
+  }
+
+  return true;
 };
 
 export const hashPassword = async (password: string) => {
@@ -67,4 +86,46 @@ export const verifyTokenUser = (req: Request, res: Response, next: NextFunction)
   } catch (error) {
     return next(error);
   }
+};
+
+export const sendConfirmationEmail = async (userId: string, userEmail: string) => {
+  const VALIDATION_EMAIL_SECRET = dotEnvConfig.VALIDATION_EMAIL_SECRET;
+  const SERVER_DOMAIN_URL = dotEnvConfig.SERVER_DOMAIN_URL;
+  const VALIDATION_EMAIL = dotEnvConfig.VALIDATION_EMAIL;
+  const MAILGUN_PRIVATE_API_KEY = dotEnvConfig.MAILGUN_PRIVATE_API_KEY;
+  const MAILGUN_DOMAIN_NAME = dotEnvConfig.MAILGUN_DOMAIN_NAME;
+
+  console.log('Sending email confirmation...');
+
+  let mg = new mailgun({ apiKey: MAILGUN_PRIVATE_API_KEY, domain: MAILGUN_DOMAIN_NAME });
+
+  jwt.sign(
+    {
+      userId: userId
+    },
+    VALIDATION_EMAIL_SECRET,
+    {
+      expiresIn: '1d'
+    },
+    (error, emailToken) => {
+      const confirmationUrl =
+        SERVER_DOMAIN_URL + `/api/v0/user/register/confirmation/${emailToken}`;
+
+      let data = {
+        from: `Cartea de Aur <${VALIDATION_EMAIL}>`,
+        to: userEmail,
+        subject: 'Email confirmation',
+        html: `Vă rugăm accesați următorul link pentru confirmarea email-ului: <a href="${confirmationUrl}">${confirmationUrl} </a>`
+      };
+
+      mg.messages().send(data, function (err, body) {
+        if (err) {
+          throw new Error(err);
+        } else {
+          console.log('sent email confirmation.');
+          console.log({ body });
+        }
+      });
+    }
+  );
 };
